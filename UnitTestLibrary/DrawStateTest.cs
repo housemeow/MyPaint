0651e5e1e596727557d05d1f43d70b7b8d91e3a1
Kai <housemeow@gmail.com>
2012-12-23 19:29:16 +0800

    add drawStateTEst and pointerStateTest

---
+++ UnitTestLibrary/DrawStateTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MyPaint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace UnitTestLibrary
+{
+    [TestClass]
+    public class DrawStateTest
+    {
+        private DrawState _drawState;
+        private PresentationModel _pModel;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _pModel = new PresentationModel(new PaintModel());
+            _drawState = new DrawState(_pModel);
+            _drawState.ShapeEnum = PaintModel.ShapeEnum.Rectangle;
+        }
+
+        [TestMethod]
+        public void TestPressPointer()
+        {
+            int triggerTime = 0;
+            _pModel._modelChanged += delegate()
+            {
+                triggerTime++;
+            };
+            _drawState.PressPointer(new Point(50, 50));
+            Assert.AreEqual(1, triggerTime);
+        }
+
+        [TestMethod]
+        public void TestMovePointer()
+        {
+            int triggerTime = 0;
+            _pModel._modelChanged += delegate()
+            {
+                triggerTime++;
+            };
+            _drawState.PressPointer(new Point(50, 50));
+            _drawState.MovePointer(new Point(100, 100));
+            Assert.AreEqual(2, triggerTime);
+        }
+
+        [TestMethod]
+        public void TestReleasePointer()
+        {
+            int triggerTime = 0;
+            _pModel._modelChanged += delegate()
+            {
+                triggerTime++;
+            };
+            _drawState.PressPointer(new Point(50, 50));
+            _drawState.MovePointer(new Point(100, 100));
+            _drawState.ReleasePointer(new Point(25, 25));
+            Assert.AreEqual(3, triggerTime);
+            Shape shape = _pModel.PaintModel.Shapes[0];
+            Assert.AreEqual(25, shape.TopLeftPoint.X);
+            Assert.AreEqual(25, shape.TopLeftPoint.Y);
+            Assert.AreEqual(50, shape.BottomRightPoint.X);
+            Assert.AreEqual(50, shape.BottomRightPoint.Y);
+        }
+    }
+}
---
+++ UnitTestLibrary/PointerStateTest.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MyPaint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace UnitTestLibrary
+{
+    [TestClass]
+    public class PointerStateTest
+    {
+        private PointerState _pointerState;
+        private PresentationModel _pModel;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _pModel = new PresentationModel(new PaintModel());
+            _pointerState = new PointerState(_pModel);
+        }
+
+        [TestMethod]
+        public void TestPressPointer()
+        {
+            int triggerTime = 0;
+            _pModel._modelChanged += delegate()
+            {
+                triggerTime++;
+            };
+            _pointerState.PressPointer(new Point(50, 50));
+            Assert.AreEqual(0, triggerTime);
+            Shape ellipse = new Ellipse();
+            ellipse.SetPoints(new Point(0, 0), new Point(100, 100));
+            _pModel.PaintModel.Add(ellipse);
+            Assert.AreEqual(1, triggerTime);
+            _pointerState.PressPointer(new Point(50, 50));
+            Assert.AreEqual(2, triggerTime);
+        }
+
+        [TestMethod]
+        public void TestMovePointer()
+        {
+            int triggerTime = 0;
+            _pModel._modelChanged += delegate()
+            {
+                triggerTime++;
+            };
+            Shape ellipse = new Ellipse();
+            ellipse.SetPoints(new Point(0, 0), new Point(100, 100));
+            _pModel.PaintModel.Add(ellipse);
+            _pointerState.PressPointer(new Point(50, 50));
+            _pointerState.MovePointer(new Point(100, 50));
+            Assert.AreEqual(3, triggerTime);
+            _pointerState.ReleasePointer(new Point(100, 50));
+            Assert.AreEqual(50, ellipse.TopLeftPoint.X);
+            Assert.AreEqual(0, ellipse.TopLeftPoint.Y);
+            Assert.AreEqual(150, ellipse.BottomRightPoint.X);
+            Assert.AreEqual(100, ellipse.BottomRightPoint.Y);
+        }
+
+        [TestMethod]
+        public void TestReleasePointer()
+        {
+            int triggerTime = 0;
+            _pModel._modelChanged += delegate()
+            {
+                triggerTime++;
+            };
+            Shape ellipse = new Ellipse();
+            ellipse.SetPoints(new Point(0, 0), new Point(100, 100));
+            _pModel.PaintModel.Add(ellipse);
+            _pointerState.PressPointer(new Point(50, 50));
+            _pointerState.MovePointer(new Point(100, 50));
+            _pointerState.ReleasePointer(new Point(100, 100));
+            Assert.AreEqual(4, triggerTime);
+            Assert.AreEqual(50, ellipse.TopLeftPoint.X);
+            Assert.AreEqual(50, ellipse.TopLeftPoint.Y);
+            Assert.AreEqual(150, ellipse.BottomRightPoint.X);
+            Assert.AreEqual(150, ellipse.BottomRightPoint.Y);
+        }
+    }
+}
